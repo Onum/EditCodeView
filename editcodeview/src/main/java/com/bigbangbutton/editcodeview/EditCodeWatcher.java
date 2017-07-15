112809872930928301c052d72c7c0f18a2940352
Alexandr Grizhinku <alexg.mobiledev@gmail.com>
2017-07-15 03:11:14 +0300

    add EditCodeWatcher / setCode fix

---
+++ editcodeview/src/main/java/com/bigbangbutton/editcodeview/EditCodeWatcher.java
@@ -0,0 +1,10 @@
+package com.bigbangbutton.editcodeview;
+
+/**
+ * Created by Alexandr Grizhinku on 15/07/2017.
+ * alexg.mobiledev@gmail.com
+ */
+
+public interface EditCodeWatcher {
+    void onCodeChanged(String code);
+}
